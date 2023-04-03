3c671c9336e7f4ac89b074e116c5636302801d59
opaulooo <o.paulooo.victor@gmail.com>
2023-04-03 16:50:06 -0300

    feat: adiciona classes

---
+++ src/main/java/com/example/Lexico.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Lexico {
+
+
+    public Lexico(Object arquivo){
+        System.out.println("");
+        return;
+    }
+
+    public Token GetToken(int l, int c){
+        return new Token();
+    }
+}
---
+++ src/main/java/com/example/Valor.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Valor {
+    private int valorInteiro;
+    private double valorDecimal;
+    private String valorIdentificador;
+
+
+    public int getvalorInteiro() {
+        return valorInteiro;
+    }
+
+    public void setvalorInteiro(int valorInteiro) {
+        this.valorInteiro = valorInteiro;
+    }
+
+    public double getValorDecimal() {
+        return valorDecimal;
+    }
+
+    public void setValorDecimal(double valorDecimal) {
+        this.valorDecimal = valorDecimal;
+    }
+
+    public String getValorIdentificador() {
+        return valorIdentificador;
+    }
+
+    public void setValorIdentificador(String valorIdentificador) {
+        this.valorIdentificador = valorIdentificador;
+    }
+
+    @Override
+    public String toString() {
+        return "Valor [valorInteiro=" + valorInteiro + ", valorDecimal=" + valorDecimal + ", valorIdentificador="
+                + valorIdentificador + "]";
+    }
+
+    
+}
